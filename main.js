201bc177a9fc7f5f366ae57a50ff5be985e6ddc4
xenogearsX <xenogears@hotmail.fr>
2020-12-14 15:34:01 +0100

    burger nav bar

---
+++ main.js
@@ -0,0 +1,17 @@
+
+
+
+const dropdownBtn = document.querySelector(".dropdown-btn");
+
+
+const dropdownMenu = document.querySelector(".dropdown-menu-content");
+
+
+
+
+dropdownBtn.addEventListener("click", function() {
+
+
+  dropdownMenu.classList.toggle("visible");
+
+});
