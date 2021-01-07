9583b7fb77dab284d992b8d3f69f52907f09302c
xenogearsX <xenogears@hotmail.fr>
2021-01-07 13:26:34 +0100

    change img url in search.js

--- js/search.js
+++ js/search.js
@@ -3,127 +3,127 @@ const cards = document.querySelector(".cards");
 const cocktailsToAdopt = [
   {
     name: "Gin Fizz",
-    picture: "../Image/ginfizz.jpg",
+    picture: "Image/ginfizz.jpg",
     url: "ginFizz.html",
     ingredients: " gin citron sucre de canne eau gazeuse"
   },
   {
     name: "Aperol Spritz",
-    picture: "../Image/aperolSpritz.jpg",
+    picture: "Image/aperolSpritz.jpg",
     url: "aperolSpritz.html",
     ingredients: "aperol prosecco soda"
   },
   {
     name: "Kir Royal",
-    picture: "../Image/kirRoyal.jpg",
+    picture: "Image/kirRoyal.jpg",
     url: "kirRoyal.html",
     ingredients: "crème de cassis champagne"
   },
   {
     name: "Manhattan",
-    picture: "../Image/manhattan.jpg",
+    picture: "Image/manhattan.jpg",
     url: "manhattan.html",
     ingredients: "whisky martini rouge angostrura bitter"
   },
   {
     name: "Gimlet",
-    picture: "../Image/gimlet.jpg",
+    picture: "Image/gimlet.jpg",
     url: "gimlet.html",
     ingredients: "gin citron jaune sirop sucre"
   },
   {
     name: "Piña colada",
-    picture: "../Image/pinaColada.jpg",
+    picture: "Image/pinaColada.jpg",
     url: "pinaColada.html",
     ingredients: "rhum ananas crème coco"
   },
   {
     name: "Hot Toddie",
-    picture: "../Image/hotToddie.jpg",
+    picture: "Image/hotToddie.jpg",
     url: "hotToddie.html",
     ingredients: "whisky citron jaune cannelle eau chaude noix de muscade miel"
   },
   {
     name: "Vodka Sour",
-    picture: "../Image/vodkaSour.jpg",
+    picture: "Image/vodkaSour.jpg",
     url: "vodkaSour.html",
     ingredients: "vodka citron jaune sirop sucre angostrura bitter œuf"
   },
   {
     name: "God Father",
-    picture: "../Image/godFather.jpg",
+    picture: "Image/godFather.jpg",
     url: "godFather.html",
     ingredients: "whisky amaretto"
   },
   {
     name: "Negroni",
-    picture: "../Image/negroni.jpg",
+    picture: "Image/negroni.jpg",
     url: "negroni.html",
     ingredients: "gin campari martini rouge"
   },
   {
     name: "Old Fashioned",
-    picture: "../Image/oldFashioned.jpg",
+    picture: "Image/oldFashioned.jpg",
     url: "oldFashioned.html",
     ingredients: "whisky angostrura bitter sirop sucre"
   },
   {
     name: "Margarita",
-    picture: "../Image/margarita.jpg",
+    picture: "Image/margarita.jpg",
     url: "margarita.html",
     ingredients: "tequila citron cointreau sirop sucre"
   },
   {
     name: "Mai Tai",
-    picture: "../Image/maiTai.jpg",
+    picture: "Image/maiTai.jpg",
     url: "maiTai.html",
     ingredients: " rhum orgeat citron falernum angostrura bitter"
   },
   {
     name: "French Martini",
-    picture: "../Image/frenchmartini.jpg",
+    picture: "Image/frenchmartini.jpg",
     url: "frenchMartini.html",
     ingredients: "vodka chambord ananas"
   },
   {
     name: "Dry Martini",
-    picture: "../Image/drymartini.jpg",
+    picture: "Image/drymartini.jpg",
     url: "dryMartini.html",
     ingredients: "vodka martini extra dry noilly prat"
   },
   {
     name: "Naked And Famous",
-    picture: "../Image/nakednfamous.jpg",
+    picture: "Image/nakednfamous.jpg",
     url: "nakedNFamous.html",
     ingredients: "mescal chartreuse jaune aperol citron"
   },
   {
     name: "Toreador",
-    picture: "../Image/toreador.jpg",
+    picture: "Image/toreador.jpg",
     url: "toreador.html",
     ingredients: "tequila liqueur abricot peychaud's bitter"
   },
   {
     name: "Paloma",
-    picture: "../Image/paloma.jpg",
+    picture: "Image/paloma.jpg",
     url: "paloma.html",
     ingredients: "tequila pamplemousse sirop sucre eau gazeuse"
   },
   {
     name: "Between The Sheets",
-    picture: "../Image/betweenTheSheets.jpg",
+    picture: "Image/betweenTheSheets.jpg",
     url: "betweenTheSheets.html",
     ingredients: "cognac citron benedectine"
   },
   {
     name: "Jack Rose",
-    picture: "../Image/jackRose.jpg",
+    picture: "Image/jackRose.jpg",
     url: "jackRose.html",
     ingredients: "calvados citron grenadine œuf"
   },
   {
     name: "Cosmopolitan",
-    picture: "../Image/cosmopolitan.jpg",
+    picture: "Image/cosmopolitan.jpg",
     url: "cosmopolitan.html",
     ingredients: "vodka triple sec cranberrie citron vert"
   }
